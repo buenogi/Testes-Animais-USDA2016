c513cb3cf94ef354c81115d29692f86db405bb0f
bueno.gi <gislayne.bueno@gmail.com>
2023-11-11 13:28:48 -0300

    Atualizaçoes do relatorio, complementaçao analitica, estetica e grafica

--- Funcoes/02_AnimalSum.R
+++ Funcoes/02_AnimalSum.R
@@ -1,12 +1,14 @@
 AnimalSum <- function(dados, especie,n_animais) {
  resumo <-  dados%>%
     group_by(especie)%>%
-    summarise(minimo = min(n_animais),
-              "1º Quantil" = quantile(n_animais, 0.25),
+    summarise("Média" = mean(n_animais),
+              "Desvio padrão" = sd(n_animais),
+              "Mínimo" = min(n_animais),
+              "1º Quartil" = quantile(n_animais, 0.25),
               "Mediana" = quantile(n_animais, 0.5),
-              "3º Quantil" = quantile(n_animais, 0.75),
-              maximo = max(n_animais),
-              media = mean(n_animais),
-              desvio = sd(n_animais))
+              "3º Quartil" = quantile(n_animais, 0.75),
+              "Máximo" = max(n_animais))
+              
+ resumo <- resumo[order(resumo$Média, decreasing = TRUE), ]
  return(resumo)
 }
